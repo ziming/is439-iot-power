035766a0557c91a7634f331f888f85f80ecfe835
Ziming Lan <ziming.lan@gmail.com>
2016-11-15 11:22:11 +0800

    url change

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
-    'allowedMethods' => ['*'],
+    'allowedMethods' => ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
     'hosts' => [],
